327dc3714274ce22902001a0ffc7f3f298f1f2d2
takashita-koichi-pana <takashita.koichi@jp.panasonic.com>
2025-05-27 01:16:46 +0000

    Add Alice API

--- main.ts
+++ main.ts
@@ -6,3 +6,7 @@ const app = new Hono()
 app.get('/', (c: Context) => {
   return c.json({ message: 'Hello World!' })
 })
+
+app.get('/alice', (c: Context) => {
+  return c.json({ message: 'Hello Alice!' })
+})
